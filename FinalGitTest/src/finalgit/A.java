d02fc4d3ce7e28d31ad2a9d9c0c3bac616f61a98
MinSeokGod <spkkang2@gmail.com>
2024-12-29 18:59:00 +0900

    A.java에 main 메서드 추가

--- FinalGitTest/src/finalgit/A.java
+++ FinalGitTest/src/finalgit/A.java
@@ -1,6 +1,11 @@
 package finalgit;
 
 public class A {
+	public static void main(String args[]) {
+		A a = new A();
+		B b = new B();
+		C c = new C();
+	}
 	public A() {
 		System.out.println("A");
 	}
